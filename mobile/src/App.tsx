f991d6a3949cbf2e8b972c89b945d15eb073236b
archiecrawford1-6895 <archie.crawford1@gmail.com>
2026-01-30 11:42:50 -0500

    update

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,14 +1,17 @@
 import "react-native-gesture-handler";
 import React from "react";
 import { StatusBar } from "expo-status-bar";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { RootNavigator } from "./navigation/RootNavigator";
 import { AppStateProvider } from "./state/AppState";
 
 export default function App() {
   return (
-    <AppStateProvider>
-      <RootNavigator />
-      <StatusBar style="dark" />
-    </AppStateProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <AppStateProvider>
+        <RootNavigator />
+        <StatusBar style="dark" />
+      </AppStateProvider>
+    </GestureHandlerRootView>
   );
 }
